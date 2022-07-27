5be52c6283acebb0e8122c01e4a3e1411a3c588f
Tim Dawes <tim.dawes@imperial.ac.uk>
2022-07-27 21:55:46 +0100

    Update Figure 3.R

--- Figure 3.R
+++ Figure 3.R
@@ -1,4 +1,4 @@
-# Pulmonary vasodilator treatment and survival in group 3 pulmonary hypertension: a Bayesian observational cohort study
+# Phosphodiesterase 5 inhibitor treatment and survival in interstitial lung disease pulmonary hypertension: a Bayesian retrospective observational cohort study
 #
 # Timothy JW Dawes [1], Colm McCabe [1,2], Konstantinos Dimopoulos [1,2,3], Iain Stewart [1], Simon Bax [2], 
 # Carl Harries [2], Chinthaka Samaranayake [1], Aleksander Kempny [1,2,3], Philip L Molyneaux [1,4],
@@ -17,59 +17,73 @@
 #
 # Correspondence details: Laura C Price. laura.price@rbht.nhs.uk
 #
-# Copyright Tim Dawes, October 2021
-#
-# Figure 3 & Supplementary Figure 5: Chose a subgroup and a treatment group to create the desired panel
-# Code set up to analyse subgroup 3.1 and effects of PDE5i (see line 33)
-
-# Choose a subgroup
-      COPD.IDn<- which(d2$Diagnosis_sub=="3.1")
-      ILD.IDn<- which(d2$Diagnosis_sub=="3.2")
-      Other.IDn<- setdiff(1:nrow(d2), union(COPD.IDn, ILD.IDn))
-      
-      IDn<- COPD.IDn # or ILD.IDn as required
-
-# Draw the plot
-      km<- with(d2[IDn,], Surv(FUTimeYears, Dead))
-      km_PDE5<- survfit(Surv(FUTimeYears, Dead) ~ PDE, data = d2[IDn,]) # Alter to ERA as needed
-      p2<- ggsurvplot(km_PDE5, data = d2[IDn,], size = 1.5,                 # change line size
-                      palette = cols[c(2,6)],# custom color palettes
-                      conf.int = TRUE,          # Add confidence interval
-                      conf.int.alpha = 0.2,
-                      pval = TRUE,              # Add p-value
-                      risk.table = TRUE,        # Add risk table
-                      risk.table.col = "strata",# Risk table color by groups
-                      censor = FALSE,
-                      surv.median.line = "hv",
-                      xlim = c(0,10),
-                      break.x.by = 1,
-                      break.y.by = 0.1,
-                      xlab = "Time (years)",
-                      legend = "none", # Change legend labels
-                      ggtheme = theme_classic()      # Change ggplot2 theme
-      )
-      dev.off()
-      p2
-
-# Output the plot
-      pdf(file="Figure3 3.1 PDE5i Rx.pdf", width=8, height=5, onefile = FALSE)
-      print(
-        p2,
-        surv.plot.height = 1,
-        risk.table.height = .25,
-        ncensor.plot.height = NULL,
-        newpage = TRUE,
-      )
-      dev.off()
-
-      
-# Find Kaplan Meier confidence intervals
-      with(d2[IDn,], round(surv.median(Surv(FUTimeYears, Dead), c(0.025, 0.5, 0.0975), 500),2))
-      km.ci(km_PDE5, method="peto")
-      
-      df<- data.frame(surv=summary(km_PDE5)$upper[1:16], time=summary(km_PDE5)$time[1:16])
-      fit<- lm(log(surv) ~ time, data=df)
-      seq(1,10,0.1)[which.min(exp(predict(fit, data.frame(time=seq(1,10,0.1)))) - 0.5)]
 
 
 
+# Figure 3
+
+# BSE Criteria for RV dysfunction
+        d2$RVdys<- unlist(sapply(1:nrow(d2), function(x) {d2[x, which(colnames(d2) == paste("RVdys.Echo", d2$Nearest.Echo.To.Dx.Number[x],sep=""))]}))
+
+           # RV dysfunction by FAC criterion
+                    d2$FAClow<- 0
+                    d2$FAClow[which(d2$Gender==0)]<- unlist(sapply(which(d2$Gender==0), function(x) {d2[x, which(colnames(d2) == paste("FAC.Echo", d2$Nearest.Echo.To.PDE.Number[x],sep=""))] < 35}))
+                    d2$FAClow[which(d2$Gender==1)]<- unlist(sapply(which(d2$Gender==1), function(x) {d2[x, which(colnames(d2) == paste("FAC.Echo", d2$Nearest.Echo.To.PDE.Number[x],sep=""))] < 30}))
+
+           # RV dysfunction by TAPSE criterion
+                    d2$TAPSElow<- sapply(1:nrow(d2), function(x) {d2[x, which(colnames(d2) == paste("TAPSE.Echo", d2$Nearest.Echo.To.PDE.Number[x],sep=""))] < 1.7})
+                    d2$TAPSElow[is.na(d2$TAPSElow) == TRUE]<- FALSE
+
+           # RV dysfunction by S' criterion
+                    d2$Slow<- sapply(1:nrow(d2), function(x) {d2[x, which(colnames(d2) == paste("S.Echo", d2$Nearest.Echo.To.PDE.Number[x],sep=""))] < 9})
+                    d2$Slow[is.na(d2$Slow) == TRUE]<- FALSE
+
+
+           # Any of the above
+                    RVdys.IDn<- which(d2$RVdys==TRUE | d2$TAPSElow==TRUE | d2$FAClow==TRUE | d2$Slow==TRUE)
+                    d2$RVdys[RVdys.IDn]<- 1
+                                      
+           # ..or those without
+                    noRVdys.IDn<- setdiff(1:nrow(d2), RVdys.IDn)
+                    d2$RVdys[noRVdys.IDn]<- 0
+                                      
+                                      
+                                      
+                                      IDn<- All.IDn
+                                      IDn<- RVdys.IDn
+                                      IDn<- noRVdys.IDn
+                                      
+                        # Choose appropriate colours
+                                      if (identical(IDn, All.IDn)) {km.cols<- c(brewer.pal(9,"Greys")[c(8)], brewer.pal(9,"Blues")[c(8)])}
+                                      if (identical(IDn, RVdys.IDn)) {km.cols<- c(brewer.pal(9,"Greys")[c(8)], brewer.pal(9,"Reds")[c(8)])}
+                                      if (identical(IDn, noRVdys.IDn)) {km.cols<- c(brewer.pal(9,"Greys")[c(8)], c(brewer.pal(9,"Greens")[c(8)]))}
+                    
+            # Plot the Kaplan Meier plot
+                        km_PDE5<- survfit(Surv(FUTimeYears, Outcome) ~ PDE, data = d2[IDn,])
+                        p2<- ggsurvplot(km_PDE5, data = d2[IDn,], size = 1.5,                 # change line size
+                                        palette = km.cols,# custom color palettes
+                                        conf.int = TRUE,          # Add confidence interval
+                                        conf.int.alpha = 0.2,
+                                        risk.table = TRUE,        # Add risk table
+                                        risk.table.col = "strata",# Risk table color by groups
+                                        legend.labs = c("PDE5i-","PDE5i+"), 
+                                        surv.median.line = "hv",
+                                        xlim = c(0,5),
+                                        break.x.by = 1,
+                                        break.y.by = 0.1,
+                                        xlab = "Time (years)",
+                                        legend = "none", # Change legend labels
+                                        gg_theme = theme_classic()
+                        ) 
+                        p2
+                        
+                        pdf(file="Figure2 Rx.pdf", width=6, height=6, onefile = FALSE)
+                        #pdf(file="Figure3 noRVDys.pdf", width=6, height=6, onefile = FALSE)
+                        
+                        p2
+                        dev.off()
+                        
+                        surv_pvalue(km_PDE5)
+                        km.ci(km_PDE5, method="peto")
+                        
+                        
