b6ed48f1d6ca84df386a65a5d3f3d3f58fa9e4af
Tim Dawes <tim.dawes@imperial.ac.uk>
2022-07-27 21:47:50 +0100

    Update Figure 2.R

--- Figure 2.R
+++ Figure 2.R
@@ -16,17 +16,13 @@
 # *Joint senior authors
 #
 # Correspondence details: Laura C Price. laura.price@rbht.nhs.uk
-#
-# Copyright Tim Dawes, October 2021
-#
 
-# Figure 2 (left panel): KM plot by PDE5+ / PDE5-  
 
-# Choose a subgroup
-      IDn<- 1:nrow(d2)
+
+# Figure 2: KM plot by PDE5+ / PDE5-  
 
 # Draw the plot
-      km<- with(d2[IDn,], Surv(FUTimeYears, Dead))
+      km<- with(d2, Surv(FUTimeYears, Dead))
       km_PDE5<- survfit(Surv(FUTimeYears, Dead) ~ PDE, data = d2[IDn,])
       p2<- ggsurvplot(km_PDE5, data = d2[IDn,], size = 1.5,                 # change line size
                       palette = cols[c(2,6)],# custom color palettes
@@ -62,88 +58,6 @@
             with(d2[IDn,], round(surv.median(Surv(FUTimeYears, Dead), c(0.025, 0.5, 0.0975), 500),2))
             km.ci(km_PDE5, method="peto")
 
-            df<- data.frame(surv=summary(km_PDE5)$upper[1:16], time=summary(km_PDE5)$time[1:16])
-            fit<- lm(log(surv) ~ time, data=df)
-            seq(1,10,0.1)[which.min(exp(predict(fit, data.frame(time=seq(1,10,0.1)))) - 0.5)]
-
-
-            
-            
-# Figure 2 (centre panel): KM plot by ERA+ / ERA-
-      km<- with(d2[IDn,], Surv(FUTimeYears, Dead))
-      km_ERA<- survfit(Surv(FUTimeYears, Dead) ~ ERA, data = d2[IDn,])
-      p<- ggsurvplot(km_ERA, data = d2[IDn,], size = 1.5,                 # change line size
-                     palette = cols[c(4,10)],# custom color palettes
-                     conf.int = TRUE,          # Add confidence interval
-                     conf.int.alpha = 0.4,
-                     pval = TRUE,              # Add p-value
-                     risk.table = TRUE,        # Add risk table
-                     risk.table.col = "strata",# Risk table color by groups
-                     legend.labs = c("ERA-", "ERA+"), 
-                     censor = FALSE,
-                     surv.median.line = "hv",
-                     xlim = c(0,10),
-                     break.x.by = 1,
-                     break.y.by = 0.1,
-                     xlab = "Time (years)",
-                     legend = "none", # Change legend labels
-                     ggtheme = theme_classic()      # Change ggplot2 theme
-      )
-      surv_median(km_ERA)
+           
       
       
-# Output the plot
-      pdf(file="Figure3 All ERA Rx.pdf", width=8, height=5, onefile = FALSE)
-      print(
-        p,
-        surv.plot.height = 1,
-        risk.table.height = .25,
-        ncensor.plot.height = NULL,
-        newpage = TRUE,
-      )
-      dev.off()
-      
-      
-# Find Kaplan Meier confidence intervals
-      km.ci(km_ERA, method="peto")
-
-
-      
-      
-
-# Figure 2 (right panel): KM plot by number of therapies
-      km<- with(d2[IDn,], Surv(FUTimeYears, Dead))
-      km_NoTherapies<- survfit(Surv(FUTimeYears, Dead) ~ NoTherapies, data = d2[IDn,])
-      p<- ggsurvplot(km_NoTherapies, data = d2[IDn,], size = 1.5,                 # change line size
-                     palette = brewer.pal(11,"Blues")[c(4,7,9)],# custom color palettes
-                     conf.int = TRUE,          # Add confidence interval
-                     conf.int.alpha = 0.4,
-                     pval = TRUE,              # Add p-value
-                     risk.table = TRUE,        # Add risk table
-                     risk.table.col = "strata",# Risk table color by groups
-                     legend.labs = c("None", "One","Two"), 
-                     censor = FALSE,
-                     surv.median.line = "hv",
-                     xlim = c(0,10),
-                     break.x.by = 1,
-                     break.y.by = 0.1,
-                     xlab = "Time (years)",
-                     legend = "none", # Change legend labels
-                     ggtheme = theme_classic()      # Change ggplot2 theme
-      )
-      
-      round(surv.median(Surv(d2[NoTherapies == 1,]$FUTimeYears, d2[NoTherapies == 1,]$Dead), c(0.025, 0.5, 0.0975), 500),2)
-      surv_median(km_NoTherapies)
-      
-
-      
-# Output the plot
-      pdf(file="Figure2 All NoTherapies.pdf", width=5, height=5, onefile = FALSE)
-      print(
-        p,
-        surv.plot.height = 1,
-        risk.table.height = .25,
-        ncensor.plot.height = NULL,
-        newpage = TRUE,
-      )
-      dev.off()      
