2b481623539b34cf70d71912bea09f105b2b7c6d
Tim Dawes <tim.dawes@imperial.ac.uk>
2022-07-27 22:06:48 +0100

    Update Supplementary Figure 2.R

--- Supplementary Figure 2.R
+++ Supplementary Figure 2.R
@@ -1,4 +1,4 @@
-# Pulmonary vasodilator treatment and survival in group 3 pulmonary hypertension: a Bayesian observational cohort study
+# Phosphodiesterase 5 inhibitor treatment and survival in interstitial lung disease pulmonary hypertension: a Bayesian retrospective observational cohort study
 #
 # Timothy JW Dawes [1], Colm McCabe [1,2], Konstantinos Dimopoulos [1,2,3], Iain Stewart [1], Simon Bax [2], 
 # Carl Harries [2], Chinthaka Samaranayake [1], Aleksander Kempny [1,2,3], Philip L Molyneaux [1,4],
@@ -16,8 +16,6 @@
 # *Joint senior authors
 #
 # Correspondence details: Laura C Price. laura.price@rbht.nhs.uk
-#
-# Copyright Tim Dawes, October 2021
 #
 # Supplementary Figure 2: Graphs of imputation accuracy by field
 
